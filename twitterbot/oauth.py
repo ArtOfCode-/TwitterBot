84bcdb270740adfa474320dcc44487bfa8c6c9c1
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-02-04 18:45:59 +0000

    complete oauth token fetching

--- twitterbot/oauth.py
+++ twitterbot/oauth.py
@@ -1,9 +1,11 @@
-import requests
-import SaveIO
 import sys
-from Config import *
+import webbrowser
+
+import requests
 import tweepy
 
+import SaveIO
+from Config import *
 
 save_subdir = "oauth"
 
@@ -11,6 +13,8 @@ data = None
 defaults = {
     'consumer_key': '7TowgTbCOSmjKSqdE41kaGHtT'
 }
+
+auth_handler = None
 
 
 def get_secret():
@@ -30,16 +34,43 @@ def get_secret():
 
 
 def on_bot_load(bot):
-    print("load")
     global data
+    global auth_handler
     data = SaveIO.load(save_subdir, "keys_data")
     if data == {}:
-        print("load new data")
         defaults['consumer_secret'] = get_secret()
         data = defaults
         SaveIO.save(data, save_subdir, "keys_data")
-    for k, v in data.items():
-        print("{0}: {1}".format(k, v))
+
+    auth_handler = tweepy.OAuthHandler(data['consumer_key'], data['consumer_secret'])
+    if "access_token" in data and "access_token_secret" in data:
+        auth_handler.set_access_token(data['access_token'], data['access_token_secret'])
+    else:
+        try:
+            redirect_url = auth_handler.get_authorization_url()
+        except tweepy.TweepError:
+            print("[oauth] [tweepy] FATAL: Failed to get authorization URL.")
+            sys.exit(503)
+        webbrowser.open_new_tab(redirect_url)
+        verifier = input("[oauth] Type the verification code from Twitter: ")
+        try:
+            access_token, access_token_secret = auth_handler.get_access_token(verifier)
+        except tweepy.TweepError:
+            print("[oauth] [tweepy] FATAL: Could not fetch access token with provided verifier code.")
+            sys.exit(504)
+        data['access_token'], data['access_token_secret'] = access_token, access_token_secret
+        SaveIO.save(data, save_subdir, "keys_data")
+        auth_handler.set_access_token(access_token, access_token_secret)
+
+
+def get_auth_handler():
+    global auth_handler
+    return auth_handler
+
+
+def get_authorized_api():
+    global auth_handler
+    return tweepy.API(auth_handler)
 
 
 def on_bot_stop(bot):
