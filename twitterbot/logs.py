86d7f11eb7ee566fd869dd72863512401f7906af
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-26 17:48:03 +0000

    fix naming conflict

--- twitterbot/logs.py
+++ twitterbot/logs.py
@@ -5,10 +5,10 @@ def write_log_line(line):
         f.write(line)
 
 def command_logs(cmd, bot, args, msg, event):
-    lines = args[0] if len(args) > 0 else 100
+    num_lines = args[0] if len(args) > 0 else 100
     with open("system.log", "r") as f:
         lines = f.readlines()
-    return "\n".join(lines[0:lines])
+    return "\n".join(lines[0:num_lines])
 
 commands = [
     Command("logs", command_logs, "Gets *n* lines of system logs. Syntax: $PREFIXlogs <lines>", False, False, None, None)
