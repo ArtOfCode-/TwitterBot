f8ec470c3d09a071f72c82d1335b9555e7dcf175
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-26 20:26:45 +0000

    read from end not start

--- twitterbot/logs.py
+++ twitterbot/logs.py
@@ -8,7 +8,7 @@ def command_logs(cmd, bot, args, msg, event):
     num_lines = int(args[0]) if len(args) > 0 else 100
     with open("system.log", "r") as f:
         lines = f.readlines()
-    return "".join(lines[0:num_lines])
+    return "".join(lines[-num_lines:])
 
 commands = [
     Command("logs", command_logs, "Gets *n* lines of system logs. Syntax: $PREFIXlogs <lines>", False, False, None, None)
