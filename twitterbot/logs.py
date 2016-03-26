9f5cff0d8f6ecd3194fd70d6963837b079e9ed96
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-26 18:06:22 +0000

    add newlines after logging

--- twitterbot/logs.py
+++ twitterbot/logs.py
@@ -2,7 +2,7 @@ from Module import Command
 
 def write_log_line(line):
     with open("system.log", "a") as f:
-        f.write(line)
+        f.write("{}\n".format(line))
 
 def command_logs(cmd, bot, args, msg, event):
     num_lines = int(args[0]) if len(args) > 0 else 100
